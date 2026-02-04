46d551963d6953d29f08afcad5fd44196a4a9b4d
Vojin <vojin.djordjevic@nike.com>
2026-02-04 14:51:22 +0100

    task-done

--- task/app.py
+++ task/app.py
@@ -7,34 +7,97 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-#TODO:
-# Create system prompt with info that it is RAG powered assistant.
-# Explain user message structure (firstly will be provided RAG context and the user question).
-# Provide instructions that LLM should use RAG Context when answer on User Question, will restrict LLM to answer
-# questions that are not related microwave usage, not related to context or out of history scope
-SYSTEM_PROMPT = """
+SYSTEM_PROMPT = """You are a RAG-powered assistant that assists users with their questions about microwave usage.
+            
+## Structure of User message:
+`RAG CONTEXT` - Retrieved documents relevant to the query.
+`USER QUESTION` - The user's actual question.
+
+## Instructions:
+- Use information from `RAG CONTEXT` as context when answering the `USER QUESTION`.
+- Cite specific sources when using information from the context.
+- Answer ONLY based on conversation history and RAG context.
+- If no relevant information exists in `RAG CONTEXT` or conversation history, state that you cannot answer the question.
 """
 
-#TODO:
-# Provide structured system prompt, with RAG Context and User Question sections.
-USER_PROMPT = """
-"""
+USER_PROMPT = """##RAG CONTEXT:
+{context}
 
 
-#TODO:
-# - create embeddings client with 'text-embedding-3-small-1' model
-# - create chat completion client
-# - create text processor, DB config: {'host': 'localhost','port': 5433,'database': 'vectordb','user': 'postgres','password': 'postgres'}
-# ---
-# Create method that will run console chat with such steps:
-# - get user input from console
-# - retrieve context
-# - perform augmentation
-# - perform generation
-# - it should run in `while` loop (since it is console chat)
+##USER QUESTION: 
+{query}"""
 
 
+embeddings_client = DialEmbeddingsClient(
+    deployment_name='text-embedding-005',
+    api_key=API_KEY
+)
+completion_client = DialChatCompletionClient(
+    deployment_name='gpt-4o',
+    api_key=API_KEY
+)
 
-# TODO:
-#  PAY ATTENTION THAT YOU NEED TO RUN Postgres DB ON THE 5433 WITH PGVECTOR EXTENSION!
-#  RUN docker-compose.yml
+text_processor = TextProcessor(
+    embeddings_client=embeddings_client,
+    db_config={
+        'host': 'localhost',
+        'port': 5433,
+        'database': 'vectordb',
+        'user': 'postgres',
+        'password': 'postgres'
+    }
+)
+
+
+def main():
+    print("🎯 Microwave RAG Assistant")
+    print("="*100)
+    load_context = input("\nLoad context to VectorDB (y/n)? > ").strip()
+    if load_context.lower().strip() in ['y', 'yes']:
+        text_processor.process_text_file(
+            file_name='embeddings/microwave_manual.txt',
+            chunk_size=400,
+            overlap=40,
+            dimensions=384
+        )
+        print("="*100)
+
+    conversation = Conversation()
+    conversation.add_message(
+        Message(Role.SYSTEM, SYSTEM_PROMPT)
+    )
+
+    while True:
+        user_request = input("\n➡️ ").strip()
+
+        if user_request.lower().strip() in ['quit', 'exit']:
+            print("👋 Goodbye")
+            break
+
+        # Step 1: Retrieval
+        print(f"{'=' * 100}\n🔍 STEP 1: RETRIEVAL\n{'-' * 100}")
+        context = text_processor.search(
+            search_mode=SearchMode.EUCLIDIAN_DISTANCE,
+            user_request=user_request,
+            top_k=5,
+            score_threshold=0.01,
+            dimensions=384
+        )
+
+        # Step 2: Augmentation
+        print(f"\n{'=' * 100}\n🔗 STEP 2: AUGMENTATION\n{'-' * 100}")
+        augmented_prompt = USER_PROMPT.format(context="\n\n".join(context), query=user_request)
+        conversation.add_message(
+            Message(Role.USER, augmented_prompt)
+        )
+        print(f"Prompt:\n{augmented_prompt}")
+
+        # Step 3: Generation
+        print(f"\n{'=' * 100}\n🤖 STEP 3: GENERATION\n{'-' * 100}")
+        ai_message = completion_client.get_completion(conversation.get_messages())
+        print(f"✅ RESPONSE:\n{ai_message.content}")
+        print("=" * 100)
+        conversation.add_message(ai_message)
+
+
+main()
--- task/embeddings/embeddings_client.py
+++ task/embeddings/embeddings_client.py
@@ -5,31 +5,58 @@ import requests
 DIAL_EMBEDDINGS = 'https://ai-proxy.lab.epam.com/openai/deployments/{model}/embeddings'
 
 
-#TODO:
-# ---
-# https://dialx.ai/dial_api#operation/sendEmbeddingsRequest
-# ---
-# Implement DialEmbeddingsClient:
-# - constructor should apply deployment name and api key
-# - create method `get_embeddings` that will generate embeddings for input list (don't forget about dimensions)
-#   with Embedding model and return back a dict with indexed embeddings (key is index from input list and value vector list)
-
 class DialEmbeddingsClient:
-    ...
+    _endpoint: str
+    _api_key: str
 
+    def __init__(self, deployment_name: str, api_key: str):
+        if not api_key or api_key.strip() == "":
+            raise ValueError("API key cannot be null or empty")
 
-# Hint:
-#  Response JSON:
-#  {
-#     "data": [
-#         {
-#             "embedding": [
-#                 0.19686688482761383,
-#                 ...
-#             ],
-#             "index": 0,
-#             "object": "embedding"
-#         }
-#     ],
-#     ...
-#  }
+        self._endpoint = DIAL_EMBEDDINGS.format(
+            model=deployment_name
+        )
+        self._api_key = api_key
+
+    def get_embeddings(
+            self, inputs: str | list[str],
+            dimensions: int,
+            print_response: bool = False
+    ) -> dict[int, list[float]]:
+        """
+        Generate dict of indexed embeddings:
+            inputs[0](text) -> [0][embedding]
+            inputs[1](text) -> [1][embedding]
+            ...
+
+        Args:
+            inputs: input text, can be singular string or list of strings
+            dimensions: number of dimensions
+            print_response: to print response in chat or not
+        """
+
+        print(f"Searching similarities for `{inputs}` \nAnd such dimensions: {dimensions}\n📋Results:\n")
+
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+        request_data = {
+            "input": inputs,
+            "dimensions": dimensions,
+        }
+
+        response = requests.post(url=self._endpoint, headers=headers, json=request_data, timeout=60)
+
+        if response.status_code == 200:
+            response_json = response.json()
+            data = response_json.get("data", [])
+            if print_response:
+                print("\n" + "=" * 50 + " RESPONSE " + "=" * 50)
+                print(json.dumps(response_json, indent=2))
+                print("=" * 108)
+            return self._from_data(data)
+        raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+    def _from_data(self, data: list[dict]) -> dict[int, list[float]]:
+        return {embedding_obj['index']: embedding_obj['embedding'] for embedding_obj in data}
